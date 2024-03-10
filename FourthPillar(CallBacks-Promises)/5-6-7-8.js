c3b78e9f0023fdfa36d73e3a6a7ee2684fbaccd4
Vamsi-Krishna8 <kothurivamsikrishna841@gmail.com>
2024-03-10 13:37:08 +0530

    new-commit

---
+++ FourthPillar(CallBacks-Promises)/5-6-7-8.js
@@ -0,0 +1,57 @@
+/*
+    ASYNC PROGRAMMING WITH JAVASCRIPT
+
+    => JavaScript is sync in nature (these all are applicable if we create valid ecmascript code which is given by the standards)
+    => JavaSctipt is single threaded.
+
+    => Example after one code part is completed then only it will go to the next part of the code it will give enough time 
+        for the code part to get complete.
+
+*/
+
+console.log("Hi");
+setTimeout(function (){
+    console.log("Timeout");
+},4000);
+console.log("Bye");
+
+/*
+    output is 
+
+    Hi
+    Bye
+    Timeout    
+        (JS dint wait at line no 13(setTimeout)) Whyy ???
+*/
+
+function timeByLoop(){
+    console.log("loop Starts");
+    for(let i=0;i<10000000000;i++){
+        //some task
+    }
+    console.log("loop ends");
+}
+
+function timeByRuntimeFeature(){
+    console.log("Starting timer");
+    setTimeout(function exec(){
+        console.log("Completed by timer");
+    },5000);
+}
+
+console.log("Hi");
+timeByLoop();
+timeByRuntimeFeature();
+timeByLoop();
+console.log("Bye");
+
+/*
+    Javascript never stop the native running ecma script code and prints someother piece of code like setTimer etc 
+    it always completes the native piece of ecmaScript code and then only it will go to other piece of code.
+
+    => It adds all the other events into the Event queue 
+    => EventLoop is the thing which always keeps on checking the callStack whether it is empty or not.
+        Eventqueue functions are executed only when the callStack is empty and no other globalCode is left.
+    => Then the task is taken from the eventloop and one by one it is added to the callStack and the process continues.
+    => EventLoop is an infiniteloop
+*/
